e1e15e2976fffb6ddb9a19810aaf232aaee6ee68
Iydon <1056736800@qq.com>
2019-02-06 11:06:07 +0800

    Update bib-cls.py

--- tools/Bibliography/bib-cls.py
+++ tools/Bibliography/bib-cls.py
@@ -9,13 +9,17 @@ class bib(object):
     AT  = "@"
     LBR = "{"
     RBR = "}"
+    SPA = " \t\n"
+    COM = ","
+    TYPE  = "type"
+    LABEL = "label"
+    INFO  = "information"
     # Variables
     result = list()
     item   = dict()
     # Functions
     def __init__(self, content:str=""):
         self.content = content
-        self.length  = len(content)
 
     def __dir__(self):
         return ["parse", "self_parse", "as_json", "self_as_json"]
@@ -57,7 +61,7 @@ class bib(object):
         """
         Parse self.
         """
-        return [self.as_json(i) for i in self.result]
+        return self.as_json(self.result)
 
     def __split_items(self):
         """
@@ -68,7 +72,7 @@ class bib(object):
         start,end = 0,0
         stack = list()
         temp  = str()
-        while start < self.length:
+        while start < len(self.content):
             if self.content[start] == self.AT:
                 while self.content[end] != self.LBR:
                     end += 1
@@ -88,8 +92,56 @@ class bib(object):
                 end += 1
 
     def __analysis_item(self, item:str):
-        return {"type":"article", "label":"aqwertyuiop",
-                "information": {"a":1, "b":2, "c":3}}
+        """
+        Analysis elements in bib item.
+        """
+        result = {self.TYPE:"", self.LABEL:"", self.INFO:dict()}
+        start,end = 0,0
+        # Type
+        while item[end+1].isalpha():
+            end += 1
+        result[self.TYPE] = item[start+1:end+1]
+        start = end+1
+        # Label
+        while item[start+1] in self.SPA:
+            start += 1
+        end = start+1
+        while item[end] not in self.SPA+self.COM:
+            end += 1
+        result[self.LABEL] = item[start+1:end]
+        start = end+1
+        # Information
+        def split(content):
+            start,mid,end = 0,0,0
+            idx = 0
+            stack = list()
+            temp  = str()
+            while start < len(content):
+                if content[start].isalpha():
+                    while content[mid].isalpha():
+                        mid += 1
+                    while content[end] != self.LBR:
+                        end += 1
+                    stack.append(self.LBR)
+                    while stack:
+                        end += 1
+                        if content[end] == self.LBR:
+                            stack.append(self.LBR)
+                        elif content[end] == self.RBR:
+                            temp = stack.pop()
+                            if temp != self.LBR:
+                                raise SyntaxError("invalid syntax")
+                    idx = content[mid:end+1].index(self.LBR)
+                    yield content[start:mid],content[mid+idx:end+1]
+                    start = end
+                    mid = end
+                else:
+                    start += 1
+                    mid += 1
+                    end += 1
+        for i,j in split(item[start:-1]):
+            result[self.INFO][i] = j[1:-1]
+        return result
 
 
 
@@ -98,4 +150,6 @@ with open("bib.bib", "r") as f:
 
 b = bib(con)
 print(b.self_parse())
-print(b.self_as_json())
+
+with open("bib.json", "w") as f:
+    f.write(b.self_as_json())
